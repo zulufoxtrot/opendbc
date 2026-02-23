66e33e1786222f1050466e7c24d477a4093565d7
zulufoxtrot <youri.lcd@gmail.com>
2026-02-23 21:21:45 +0100

    hopefully fix LIMIT bug

--- opendbc/car/hyundai/carstate.py
+++ opendbc/car/hyundai/carstate.py
@@ -134,7 +134,7 @@ class CarState(CarStateBase, EsccCarStateBase, MadsCarState, CarStateExt):
       ret.cruiseState.nonAdaptive = False
     elif not self.CP_SP.flags & HyundaiFlagsSP.NON_SCC:
       ret.cruiseState.available = cp_cruise.vl["SCC11"]["MainMode_ACC"] == 1
-      ret.cruiseState.enabled = cp_cruise.vl["SCC12"]["ACCMode"] != 0
+      ret.cruiseState.enabled = cp_cruise.vl["SCC12"]["ACCMode"] == 1
       ret.cruiseState.standstill = cp_cruise.vl["SCC11"]["SCCInfoDisplay"] == 4.
       ret.cruiseState.nonAdaptive = cp_cruise.vl["SCC11"]["SCCInfoDisplay"] == 2.  # Shows 'Cruise Control' on dash
       ret.cruiseState.speed = cp_cruise.vl["SCC11"]["VSetDis"] * speed_conv
