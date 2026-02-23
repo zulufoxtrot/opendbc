8e0aa43a0846aa0268f9554abecb619a44b2f924
zulufoxtrot <youri.lcd@gmail.com>
2026-02-23 21:44:12 +0100

    set STEER_DELTA_DOWN back to default

--- opendbc/car/hyundai/values.py
+++ opendbc/car/hyundai/values.py
@@ -42,10 +42,8 @@ class CarControllerParams:
       self.STEER_MAX = 255
 
     elif CP.carFingerprint in (CAR.KIA_NIRO_EV):
-      # random values for now, needs testing
       self.STEER_MAX = 409
       self.STEER_DELTA_UP = 6  # increase from 3 to 6
-      self.STEER_DELTA_DOWN = 5 # lower than default, to hopefully prevent "blocky" wheel releases after curves
 
     # these cars have significantly more torque than most HKG; limit to 70% of max
     elif CP.flags & HyundaiFlags.ALT_LIMITS:
